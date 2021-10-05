01500be9fd93aab3fa8ee088664c31f2ff2badbd
divyaganapathi <91954957+divyaganapathi@users.noreply.github.com>
2021-10-05 18:56:09 +0100

    Update PageObject.java

--- src/test/java/PageObject.java
+++ src/test/java/PageObject.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.support.PageFactory;
 
 public class PageObject {
     protected WebDriver Driver;
+    public PageObject(){
+    }
     public PageObject (WebDriver driver){
         this.Driver=driver;
         PageFactory.initElements(Driver,this);
